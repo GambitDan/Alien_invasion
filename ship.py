54d74bcdf1fd07e7d1bcc7e61f0f666a34fe7d31
Daniel Houghton <danielhoughton1986@gmail.com>
2023-09-04 22:17:17 +0100

    added fullscreen mode and quit with 'q'

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         "Initialise the ship and set its starting position"
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load("images/ship.bmp")
@@ -16,16 +17,22 @@ class Ship:
         # Start each new ship at the bottom centre of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # store a float for the ships exact horizontal position
+        self.x = float(self.rect.x)
+
         # movement flag; start with ship thats not moving.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """update the ships position based on the movement flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
